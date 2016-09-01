1797d2f9907e2bd4b46c89f1fe55cb7e86115d12
Jermski <JDBranham@Hotmail.com>
2016-08-31 20:59:00 -0500

    updated cnfig

--- wp-config.php
+++ wp-config.php
@@ -14,21 +14,21 @@
  * @package WordPress
  */
 
-// ** Heroku Postgres settings - from Heroku Environment ** //
+// ** Heroku MySQL settings - from Heroku Environment ** //
 $db = parse_url($_ENV["DATABASE_URL"]);
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', trim($db["path"],"/"));
+define('DB_NAME', getenv('DB_NAME'));
 
 /** MySQL database username */
-define('DB_USER', $db["user"]);
+define('DB_USER', getenv('DB_USER'));
 
 /** MySQL database password */
-define('DB_PASSWORD', $db["pass"]);
+define('DB_PASSWORD', getenv('DB_PASS'));
 
 /** MySQL hostname */
-define('DB_HOST', $db["host"]);
+define('DB_HOST', getenv('DB_HOST'));
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
